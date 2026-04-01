7bcb47baa87834348eee05e9ce4fe3c6687e4b7d
Bijay Devkota <60361179+bzay1992@users.noreply.github.com>
2026-04-01 09:56:46 -0700

    Update databricks:notebooks:01_bronze_ingestion.py

--- databricks:notebooks:01_bronze_ingestion.py
+++ databricks:notebooks:01_bronze_ingestion.py
@@ -1,18 +1,11 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+from pyspark.sql import SparkSession
 
-\f0\fs24 \cf0 from pyspark.sql import SparkSession\
-\
-spark = SparkSession.builder.appName("BronzeIngestion").getOrCreate()\
-\
-df = spark.read.format("csv") \\\
-    .option("header", "true") \\\
-    .load("/mnt/adls/bronze/claims_raw.csv")\
-\
-df.write.format("delta") \\\
-    .mode("overwrite") \\\
-    .save("/mnt/adls/bronze/claims")}
+spark = SparkSession.builder.appName("BronzeIngestion").getOrCreate()
+
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .load("/mnt/adls/bronze/claims_raw.csv")
+
+df.write.format("delta") \
+    .mode("overwrite") \
+    .save("/mnt/adls/bronze/claims")
